fc94e43c98405f583d9df933bcd7b208c71d1079
Tarang <tarangbhavsar@gmail.com>
2021-06-22 23:53:01 -0400

    Initial Commit

---
+++ src/main/java/com/thunder/gorgeousgraphql/GorgeousGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.thunder.gorgeousgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorgeousGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorgeousGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thunder/gorgeousgraphql/GorgeousGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thunder.gorgeousgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorgeousGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
